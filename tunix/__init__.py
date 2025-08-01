aba20d4ec131f90583cfb4776494e58e6478a4d2
Tianshu Bao <tsbao@google.com>
2025-08-01 13:38:52 -0700

    fix g3 import
    PiperOrigin-RevId: 789879354

---
+++ tunix/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tunix API."""
+
+# pylint: disable=g-multiple-import, g-importing-member
+
+from tunix.distillation.distillation_trainer import DistillationTrainer, TrainingConfig as DistillationTrainingConfig
+from tunix.generate.sampler import CacheConfig, Sampler
+from tunix.rl.dpo.dpo_trainer import DpoTrainer, DpoTrainingConfig
+from tunix.rl.grpo.grpo_learner import GrpoConfig, GrpoLearner
+from tunix.rl.rl_cluster import ClusterConfig, RLCluster, RLTrainingConfig, Role
+from tunix.sft.peft_trainer import PeftTrainer, TrainingConfig
+
+# pylint: enable=g-multiple-import, g-importing-member
