d799a45d48027e27b6a08aaf7cb15e6a4f495c01
Lin Chai <linchai@google.com>
2025-08-12 09:36:22 -0700

    [Tunix] add qwen2 weights mappings to HF.
    PiperOrigin-RevId: 794145972

--- tunix/models/qwen2/model.py
+++ tunix/models/qwen2/model.py
@@ -573,3 +573,150 @@ class Qwen2(nnx.Module):
       logits = self.lm_head(x)
 
     return logits, new_cache  # pytype: disable=bad-return-type
+
+  @staticmethod
+  def to_hf_mappings():
+    return {
+        'embedder.input_embedding': ('embed.embedding', ('model', None)),
+        'layers.*.input_layernorm.w': (
+            'model.layers.*.input_layernorm.scale',
+            (None,),
+        ),
+        'layers.*.mlp.down_proj.kernel': (
+            'model.layers.*.mlp.down_proj.kernel',
+            ('model', None),
+        ),
+        'layers.*.mlp.gate_proj.kernel': (
+            'model.layers.*.mlp.gate_proj.kernel',
+            (None, 'model'),
+        ),
+        'layers.*.mlp.up_proj.kernel': (
+            'model.layers.*.mlp.up_proj.kernel',
+            (None, 'model'),
+        ),
+        'layers.*.post_attention_layernorm.w': (
+            'model.layers.*.post_attention_layernorm.scale',
+            (None,),
+        ),
+        'layers.*.attn.k_proj.w': (
+            'model.layers.*.self_attn.k_proj.kernel',
+            (None, 'model', None),
+        ),
+        'layers.*.attn.o_proj.w': (
+            'model.layers.*.self_attn.o_proj.kernel',
+            ('model', None, None),
+        ),
+        'layers.*.attn.q_proj.w': (
+            'model.layers.*.self_attn.q_proj.kernel',
+            (None, 'model', None),
+        ),
+        'layers.*.attn.v_proj.w': (
+            'model.layers.*.self_attn.v_proj.kernel',
+            (None, 'model', None),
+        ),
+        'layers.*.attn.q_bias': (
+            'model.layers.*.self_attn.q_proj.bias',
+            ('model', None),
+        ),
+        'layers.*.attn.k_bias': (
+            'model.layers.*.self_attn.k_proj.bias',
+            ('model', None),
+        ),
+        'layers.*.attn.v_bias': (
+            'model.layers.*.self_attn.v_proj.bias',
+            ('model', None),
+        ),
+        'final_norm.w': ('model.norm.scale', (None,)),
+        'lm_head': ('lm_head', (None, 'model')),
+    }
+
+  @staticmethod
+  def lora_to_hf_mappings():
+    return {
+        'layers.*.mlp.gate_proj.kernel_lora_a': (
+            'model.layers.*.mlp.gate_proj.kernel_lora_a',
+            (None, None),
+        ),
+        'layers.*.mlp.gate_proj.kernel_lora_b': (
+            'model.layers.*.mlp.gate_proj.kernel_lora_b',
+            (None, 'model'),
+        ),
+        'layers.*.mlp.up_proj.kernel_lora_a': (
+            'model.layers.*.mlp.up_proj.kernel_lora_a',
+            (None, None),
+        ),
+        'layers.*.mlp.up_proj.kernel_lora_b': (
+            'model.layers.*.mlp.up_proj.kernel_lora_b',
+            (None, 'model'),
+        ),
+        'layers.*.mlp.down_proj.kernel_lora_a': (
+            'model.layers.*.mlp.down_proj.kernel_lora_a',
+            ('model', None),
+        ),
+        'layers.*.mlp.down_proj.kernel_lora_b': (
+            'model.layers.*.mlp.down_proj.kernel_lora_b',
+            (None, None),
+        ),
+        'layers.*.attn.q_proj.w_lora_a': (
+            'layers.*.self_attn.q_proj.kernel_lora_a',
+            ('model', None),
+        ),
+        'layers.*.attn.q_proj.w_lora_b': (
+            'layers.*.self_attn.q_proj.kernel_lora_b',
+            (None, None),
+        ),
+        'layers.*.attn.k_proj.w_lora_a': (
+            'layers.*.self_attn.k_proj.kernel_lora_a',
+            ('model', None),
+        ),
+        'layers.*.attn.k_proj.w_lora_b': (
+            'layers.*.self_attn.k_proj.kernel_lora_b',
+            (None, None),
+        ),
+        'layers.*.attn.v_proj.w_lora_a': (
+            'layers.*.self_attn.v_proj.kernel_lora_a',
+            ('model', None),
+        ),
+        'layers.*.attn.v_proj.w_lora_b': (
+            'layers.*.self_attn.v_proj.kernel_lora_b',
+            (None, None),
+        ),
+        'layers.*.attn.o_proj.w_lora_a': (
+            'layers.*.self_attn.o_proj.kernel_lora_a',
+            ('model', None),
+        ),
+        'layers.*.attn.o_proj.w_lora_b': (
+            'layers.*.self_attn.o_proj.kernel_lora_b',
+            (None, None),
+        ),
+        'layers.*.attn.q_bias_lora_a': (
+            'model.layers.*.self_attn.q_proj.bias_lora_a',
+            ('model', None),
+        ),
+        'layers.*.attn.q_bias_lora_b': (
+            'model.layers.*.self_attn.q_proj.bias_lora_b',
+            ('model', None),
+        ),
+        'layers.*.attn.k_bias_lora_a': (
+            'model.layers.*.self_attn.k_proj.bias_lora_a',
+            ('model', None),
+        ),
+        'layers.*.attn.k_bias_lora_b': (
+            'model.layers.*.self_attn.k_proj.bias_lora_b',
+            ('model', None),
+        ),
+        'layers.*.attn.v_bias_lora_a': (
+            'model.layers.*.self_attn.v_proj.bias_lora_a',
+            ('model', None),
+        ),
+        'layers.*.attn.v_bias_lora_b': (
+            'model.layers.*.self_attn.v_proj.bias_lora_b',
+            ('model', None),
+        ),
+    }
+
+  @staticmethod
+  def to_hf_transpose_keys():
+    return {
+        'embedding': (1, 0),
+    }
