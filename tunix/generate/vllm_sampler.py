1c12ec5e5a67c47d134849804219fc675a0aebfc
Tianshu Bao <tsbao@google.com>
2025-08-23 19:46:44 +0000

    partial fix vllm

--- tunix/generate/vllm_sampler.py
+++ tunix/generate/vllm_sampler.py
@@ -27,6 +27,7 @@ from tunix.generate import utils
 import tunix.generate.tokenizer_adapter as tok_adapter
 from tunix.rl import reshard
 from vllm import LLM
+from vllm.inputs import TokensPrompt
 from vllm.outputs import RequestOutput
 
 
@@ -240,8 +241,7 @@ class VllmSampler(base_sampler.BaseSampler):  # pylint: disable=invalid-name
 
     prompt_ids = [self.tokenize(x) for x in input_strings]
     outputs = self.llm.generate(
-        prompts=None,
-        prompt_token_ids=prompt_ids,
+        prompts=[TokensPrompt(prompt_token_ids=ids) for ids in prompt_ids],
         sampling_params=self.sampling_params,
         use_tqdm=True,
     )
